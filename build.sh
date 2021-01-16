de7184b6afbc9b42ea58bedd07a9c004bb4b7ee8
johandegn <degnjohan@gmail.com>
2021-01-16 12:44:29 +0100

    Changed folder structure. Needs more work

--- build.sh
+++ build.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
+cd src
 
 ocamlopt -c tokens.ml nodes.ml
 ocamlc -c lexer.mli parser.mli interpreter.mli
 ocamlopt -o brainfuck lexer.ml parser.ml interpreter.ml brainfuck.ml
 
 rm *.cmi *.cmx *.o
+
+mv brainfuck ../
+
+cd ..
