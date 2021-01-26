b188c416e6c8fac4aee80d69f6daef602521eb5d
johandegn <degnjohan@gmail.com>
2021-01-26 22:50:41 +0100

    Memory dump formats negative numbers correctly

--- src/memory.ml
+++ src/memory.ml
@@ -2,15 +2,21 @@ type memory = { l : int list; c : int; r : int list; ptr : int }
 
 
 let output_line idx idx_digits ncells lst ptr =
-  let fixed_length length chr str =
-    let amount = length - (String.length str) in
-    if amount < 0 then (* well, can't fit it in... *)
-      String.make length '?'
+  let fixed_length length chr num =
+    let str1 = string_of_int (abs num) in
+    let real_length = length - (if num < 0 then 1 else 0) in
+    let amount = real_length - (String.length str1) in
+    let str2 = if amount < 0 then (* well, can't fit it in... *)
+      String.make real_length '?'
     else
-      Printf.sprintf "%s%s" (String.make amount chr) str in
+      Printf.sprintf "%s%s" (String.make amount chr) str1 in
+    if num < 0 then
+      "-" ^ str2
+    else
+      str2 in
 
   (* print index *)
-  Printf.printf "%s:  " (fixed_length idx_digits '0' (string_of_int idx));
+  Printf.printf "%s:  " (fixed_length idx_digits '0' idx);
 
   (* print cells *)
   let rec print_next i lst =
@@ -21,7 +27,7 @@ let output_line idx idx_digits ncells lst ptr =
       | l'::ls' -> (l', ls')
       | [] -> (0, []) in
       let (c1, c2) = if ptr = i + idx then ('[', ']') else (' ',' ') in (* mark ptr location *)
-      Printf.printf "%c%s%c" c1 (fixed_length 3 '0' (string_of_int l)) c2; (* FIXME: magic constant 3 *)
+      Printf.printf "%c%s%c" c1 (fixed_length 3 '0' l) c2; (* FIXME: magic constant 3 *)
       let (rlst, cs) = print_next (i + 1) ls in (rlst, l::cs) in
 
   let (res_lst, cells) = print_next 0 lst in
@@ -41,7 +47,7 @@ let output_line idx idx_digits ncells lst ptr =
 
 
 let print_memory mem =
-  let ncells = 16 in
+  let ncells = 15 in
   let lidx = List.length mem.l in
   let lst = List.rev_append mem.l (mem.c::mem.r) in
   let total_elms = List.length lst in
