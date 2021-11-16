188cbc9231dd854f148038ad01ff88f05d957226
Johan Degn <degnjohan@gmail.com>
2021-11-16 23:48:49 +0100

    Reworked lexer

--- src/lexer.ml
+++ src/lexer.ml
@@ -1,39 +1,37 @@
-let rec create_tokens clst = 
-  let get_token c = 
-    match c with
-    | ',' -> Tokens.Input
-    | '.' -> Tokens.Output
-    | '+' -> Tokens.Increase
-    | '-' -> Tokens.Decrease
-    | '<' -> Tokens.LeftShift
-    | '>' -> Tokens.RightShift
-    | '[' -> Tokens.OpenBracket
-    | ']' -> Tokens.CloseBracket
-    | _ -> failwith "char array is not clean!" in (* shorthand for Failure exception*)
-  match clst with
-  | c::l -> (get_token c)::(create_tokens l)
-  | [] -> []
+open Tokens
 
 let valid_symbol c =
   match c with
-  | ',' -> true
-  | '.' -> true
-  | '+' -> true
-  | '-' -> true
-  | '<' -> true
-  | '>' -> true
-  | '[' -> true
+  | ','
+  | '.'
+  | '+'
+  | '-'
+  | '<'
+  | '>'
+  | '['
   | ']' -> true
   | _ -> false
-  
-let cleanup str =
+
+let get_token c = 
+  match c with
+  | ',' -> Input
+  | '.' -> Output
+  | '+' -> Increase
+  | '-' -> Decrease
+  | '<' -> LeftShift
+  | '>' -> RightShift
+  | '[' -> OpenBracket
+  | ']' -> CloseBracket
+  | _ -> failwith "char array is not clean!" (* shorthand for Failure exception*)
+
+let create_tokens str =
   let max = String.length str - 1 in
-  if max < 0 then [] else (* if str is empty *)
-  let rec clean i =
-    let c = str.[i] in
-    let tail = (if i = max then [] else (clean (i + 1))) in
-    if valid_symbol c then c::tail else tail in
-  clean 0
+  let rec inner n tail =
+    if n < 0 then tail else
+    let c = str.[n] in
+    if not @@ valid_symbol c then inner (n - 1) tail else
+    get_token c :: tail |> inner (n - 1) in
+  inner max []
 
 let tokenize str =
-  create_tokens (cleanup str);;
+  create_tokens str
